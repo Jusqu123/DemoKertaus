69763fb4d4a4227ea68bab470d78fb7ef14930f5
Jusqu123 <jusquu123@gmail.com>
2016-02-12 15:54:33 +0200

    all aboard!!!

---
+++ DemoKertaus/Katsoja.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoKertaus
+{
+    class Katsoja : Henkilo ,Fani
+    {
+        public bool Kausikortti { get; set; }
+
+
+        public Katsoja()
+        {
+
+        }
+        public Katsoja(string nimi, double paino,bool kausikortti)
+            :base(nimi,paino)
+        {
+            Kausikortti = kausikortti;
+        }
+        public override string ToString()
+        {
+            //NIMI+PAINO  + KAUSIKORTTI
+            return base.ToString()+ Kausikortti;
+        }
+
+        public void huutaa()
+        {
+            Console.WriteLine("SATAMAAN!!!!!!");
+        }
+    }
+}
---
+++ DemoKertaus/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoKertaus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Henkilo henkilo = new Henkilo();
+            henkilo.Nimi = "Nickkclass Helmenkalastaja";
+            henkilo.Paino = 60;
+            Console.WriteLine("henkilön nimi : " + henkilo.Nimi);
+            Console.WriteLine("Henkilön paino : " + henkilo.Paino);
+
+
+            string nimi = "Nikke Kalastaja";
+            double paino = 50;
+
+            Henkilo henkilo2 = new Henkilo(nimi, paino);
+            Console.WriteLine("henkilön nimi : " + henkilo2.Nimi);
+            Console.WriteLine("Henkilön paino : " + henkilo2.Paino);
+
+            Henkilo henkilo3 = new Henkilo { Nimi = "Kapteeni Mustaparta", Paino = 100 };
+            Console.WriteLine("henkilön nimi : " + henkilo3.Nimi);
+            Console.WriteLine("Henkilön paino : " + henkilo3.Paino);
+
+            Katsoja katsoja = new Katsoja("Ezio Auditore Da Firenze ",100 ,true);
+            Console.WriteLine(katsoja.ToString());
+            katsoja.huutaa();
+        }
+    }
+}
